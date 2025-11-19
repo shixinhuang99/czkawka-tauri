20239aa545ce75b40d6d9449b2aa303837a4dec2
Shixin Huang <shixinhuang99@gmail.com>
2025-11-19 23:41:16 +0800

    fix migrate logic

--- ui/src/utils/migrate.ts
+++ ui/src/utils/migrate.ts
@@ -1,5 +1,7 @@
 import {
   CURRENT_TOOL_KEY,
+  LANGUAGE_KEY,
+  Languages,
   SETTINGS_PRESETS_KEY,
   THEME_KEY,
   Theme,
@@ -7,33 +9,54 @@ import {
 } from '~/consts';
 import { isValidTool } from './common';
 
+function getStringifyValue(jsonString: string | null): string | null {
+  if (jsonString) {
+    try {
+      const value = JSON.parse(jsonString);
+      if (typeof value === 'string') {
+        return value;
+      }
+    } catch (_) {}
+  }
+  return null;
+}
+
+function getStringValue(jsonString: string | null): string | null {
+  if (jsonString) {
+    try {
+      if (typeof jsonString === 'string') {
+        return jsonString;
+      }
+    } catch (_) {}
+  }
+  return null;
+}
+
 export function migrate() {
   currentToolMigrate();
   settingPresetsMigrate();
   themeMigrate();
+  languageMigrate();
 }
 
 function currentToolMigrate() {
-  const storedValue = localStorage.getItem(CURRENT_TOOL_KEY);
+  const currentTool = getStringifyValue(localStorage.getItem(CURRENT_TOOL_KEY));
 
-  if (!storedValue) {
-    localStorage.setItem(CURRENT_TOOL_KEY, Tools.DuplicateFiles);
-    return;
-  }
-
-  if (!isValidTool(storedValue)) {
-    localStorage.setItem(CURRENT_TOOL_KEY, Tools.DuplicateFiles);
+  if (!currentTool || !isValidTool(currentTool)) {
+    localStorage.setItem(
+      CURRENT_TOOL_KEY,
+      JSON.stringify(Tools.DuplicateFiles),
+    );
   }
 }
 
 function settingPresetsMigrate() {
-  const OLD_PRESETS_KEY = 'setting-presets';
-  const oldValue = localStorage.getItem(OLD_PRESETS_KEY);
-  const newValue = localStorage.getItem(SETTINGS_PRESETS_KEY);
-
-  if (newValue) {
+  if (localStorage.getItem(SETTINGS_PRESETS_KEY)) {
     return;
   }
+
+  const OLD_PRESETS_KEY = 'setting-presets';
+  const oldValue = localStorage.getItem(OLD_PRESETS_KEY);
 
   if (oldValue) {
     localStorage.setItem(SETTINGS_PRESETS_KEY, oldValue);
@@ -42,22 +65,23 @@ function settingPresetsMigrate() {
 }
 
 function themeMigrate() {
-  const storedValue = localStorage.getItem(THEME_KEY);
+  const theme = getStringValue(localStorage.getItem(THEME_KEY));
 
-  if (!storedValue) {
-    localStorage.setItem(THEME_KEY, Theme.System);
+  if (theme && Object.values<string>(Theme).includes(theme)) {
+    localStorage.setItem(THEME_KEY, JSON.stringify(theme));
     return;
   }
 
-  try {
-    const parsedValue = JSON.parse(storedValue);
-    if (
-      typeof parsedValue !== 'string' ||
-      !Object.values<string>(Theme).includes(parsedValue)
-    ) {
-      localStorage.setItem(THEME_KEY, Theme.System);
-    }
-  } catch (_) {
-    localStorage.setItem(THEME_KEY, Theme.System);
+  localStorage.setItem(THEME_KEY, JSON.stringify(Theme.System));
+}
+
+function languageMigrate() {
+  const language = getStringValue(localStorage.getItem(LANGUAGE_KEY));
+
+  if (language && Object.values<string>(Languages).includes(language)) {
+    localStorage.setItem(LANGUAGE_KEY, JSON.stringify(language));
+    return;
   }
+
+  localStorage.setItem(LANGUAGE_KEY, JSON.stringify(Languages.En));
 }
